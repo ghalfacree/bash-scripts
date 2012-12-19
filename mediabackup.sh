c7e84a17bd4c08220cd978adb5bba621f0962634
Gareth Halfacree <gareth@halfacree.co.uk>
2012-12-19 10:30:47 +0000

    Sick of doing this manually.

---
+++ mediabackup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rsync -av --progress /media/Data/Photos/ blacklaw.homedns.org:/media/Seagate320/Photos/
+rsync -av --progress /media/Data/My\ Music/ blacklaw.homedns.org:/media/Seagate320/Music/
