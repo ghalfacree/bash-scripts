c678d83ae833488a75f989e4315fbfc2938fd642
Gareth Halfacree <gareth@halfacree.co.uk>
2013-02-27 09:11:12 +0000

    Automatically compress and move old Expert Reviews articles

--- dropbox.sh
+++ dropbox.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-rsync -av --progress --delete /home/blacklaw/Dropbox/ blacklaw.homedns.org:/media/Seagate320/Dropbox/
+if [ -e /home/blacklaw/Dropbox/Work/expertreviews/articles/*txt ]
+then
+  bzip2 /home/blacklaw/Dropbox/Work/expertreviews/articles/*txt && mv ~/Dropbox/Work/expertreviews/articles/*bz2 ~/Dropbox/Work/expertreviews/articles/old/
+fi
+rsync -e "ssh -c arcfour" -av --progress --delete /home/blacklaw/Dropbox/ blacklaw.homedns.org:/media/Seagate320/Dropbox/
