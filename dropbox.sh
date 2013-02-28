1113b89d3128a4190d432ee468813a253acfe2bd
Gareth Halfacree <gareth@halfacree.co.uk>
2013-02-28 09:28:09 +0000

    Properly fixed file test this time. Probably.

--- dropbox.sh
+++ dropbox.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ `ls /home/blacklaw/Dropbox/Work/expertreviews/articles/*txt 2> /dev/null | wc -l` > 0 ]
+if ls -1 /home/blacklaw/Dropbox/Work/expertreviews/articles/*txt > /dev/null 2>&1
 then
   bzip2 /home/blacklaw/Dropbox/Work/expertreviews/articles/*txt && mv ~/Dropbox/Work/expertreviews/articles/*bz2 ~/Dropbox/Work/expertreviews/articles/old/
 fi
