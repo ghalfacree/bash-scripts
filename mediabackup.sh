2c3ee706efe68da5adf176caa4a1067b4d411fe7
Gareth Halfacree <gareth@halfacree.co.uk>
2013-03-06 10:07:15 +0000

    Updated for new drive layout, added video backup

--- mediabackup.sh
+++ mediabackup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 echo Updating local Nexus 4 image archive...
-rsync -e "ssh -c arcfour" -av --progress blacklaw.homedns.org:/media/Seagate320/NexusBackup/DCIM/ /media/Data/Photos/Nexus\ 4/DCIM/
+rsync -e "ssh -c arcfour" -av --progress blacklaw.homedns.org:/media/ExternalDrives/NexusBackup/DCIM/ /media/Data/Photos/Nexus\ 4/DCIM/
 echo Synchronising remote photo archive...
-rsync -e "ssh -c arcfour" -av --delete --progress /media/Data/Photos/ blacklaw.homedns.org:/media/Seagate320/Photos/
+rsync -e "ssh -c arcfour" -av --delete --progress /media/Data/Photos/ blacklaw.homedns.org:/media/ExternalDrives/Photos/
 echo Synchronising remote music archive...
-rsync -e "ssh -c arcfour" -av --delete --progress /media/Data/My\ Music/ blacklaw.homedns.org:/media/Seagate320/Music/
+rsync -e "ssh -c arcfour" -av --delete --progress /media/Data/My\ Music/ blacklaw.homedns.org:/media/ExternalDrives/Music/
+echo "Synchronising remote video archive..."
+rsync -e "ssh -c arcfour" -av --delete --progress /media/Data/Videos/ blacklaw.homedns.org:/media/ExternalDrives/Videos/
 echo Backup complete.
