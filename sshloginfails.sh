7f7d60d3acd12f3345d94a3d25ab84d6b085ac69
Gareth Halfacree <gareth@halfacree.co.uk>
2013-03-13 11:58:17 +0000

    Simplified thanks to @TomNomNom

--- sshloginfails.sh
+++ sshloginfails.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Check the number of failed logins in auth.log
 # If you're running anything other than OpenSSH 1.6, this will likely need changing
-
-for i in `grep invalid /var/log/auth.log | cut -d' ' -f9 | sort | uniq`; do echo -ne $i\ ; grep \ $i\  /var/log/auth.log | grep invalid | wc -l; done
+grep invalid /var/log/auth.log | cut -d' ' -f9 | sort | uniq -c
