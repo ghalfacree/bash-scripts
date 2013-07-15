7a9d7ad64591f9f206dcca7e4fbaa319fc757b84
Gareth Halfacree <gareth@halfacree.co.uk>
2013-07-15 17:00:43 +0100

    Convert to 7-bit ASCII.

--- nc100send.sh
+++ nc100send.sh
@@ -5,6 +5,10 @@ if [ "$1" = "" ]; then
   echo USAGE: nc100send.sh filename
   exit
 fi
+echo Converting $1 to 7-bit ASCII...
+iconv -f UTF-8 -t ASCII//TRANSLIT -o /tmp/nc100.ascii $1
 echo Sending $1...
-sx $1 > /dev/ttyWCH0 < /dev/ttyWCH0
+sx /tmp/nc100.ascii > /dev/ttyWCH0 < /dev/ttyWCH0
+echo Cleaning up...
+rm /tmp/nc100.ascii
 echo File $1 transferred successfully.
