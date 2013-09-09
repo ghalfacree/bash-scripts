47ab4050c89375e3349f9309bd1ced38cdaa7ec8
Gareth Halfacree <gareth@halfacree.co.uk>
2013-09-09 23:44:06 +0100

    Turns out the first method was better.

--- macfix.sh
+++ macfix.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Fix for stupid mDNS resolution breaking in Mac OS X. Stupid thing.
-ifconfig en0 down
-ifconfig en0 up
+launchctl unload -w /System/Library/LaunchDaemons/com.apple.mDNSResponder.plist
+launchctl load -w /System/Library/LaunchDaemons/com.apple.mDNSResponder.plist
 echo It may be fixed. Or possibly not. Good luck!
