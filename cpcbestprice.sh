823addc7aa98bc99ed51ff5cf9219809f90f9304
Gareth Halfacree <gareth@halfacree.co.uk>
2013-12-12 16:25:13 +0000

    Print savings amount in pounds or pence

--- cpcbestprice.sh
+++ cpcbestprice.sh
@@ -33,14 +33,15 @@ if [ "$bestprice" == "" ]; then
 fi
 printf " £$bestprice found.\n"
 winningcode=$(echo $1 at £$bestprice.)
+originalpricepence=$(echo $bestprice | sed -e 's/\.//g' -e 's/^0*//')
 
 for i in {0..9}; do
 	searchsuffix="0$i"
 	printf "\rTesting product code $baseproductcode$searchsuffix..."
 	currentprice=$(wget -q -O - "http://cpc.farnell.com/jsp/search/productdetail.jsp?_dyncharset=UTF-8&searchTerms=$baseproductcode$searchsuffix&_D%3AsearchTerms=+&%2Fpf%2Fsearch%2FTextSearchFormHandler.search=GO&_D%3A%2Fpf%2Fsearch%2FTextSearchFormHandler.search=+&s=&%2Fpf%2Fsearch%2FTextSearchFormHandler.suggestions=false&_D%3A%2Fpf%2Fsearch%2FTextSearchFormHandler.suggestions=+&%2Fpf%2Fsearch%2FTextSearchFormHandler.ref=globalsearch&_D%3A%2Fpf%2Fsearch%2FTextSearchFormHandler.ref=+&_D%3A%2Fpf%2Fsearch%2FTextSearchFormHandler.onlyInStockProducts=+&%2Fpf%2Fsearch%2FTextSearchFormHandler.onlyInStockProductsActive=true&_D%3A%2Fpf%2Fsearch%2FTextSearchFormHandler.onlyInStockProductsActive=+&_DARGS=%2Fjsp%2Fcommonfragments%2FglobalsearchE14.jsp" | grep taxedvalue -m 1 | cut -d" " -f1 | sed 's/£//g')
 	if [ "$currentprice" != "" ]; then
-		currentpricepence=$(echo $currentprice | sed 's/\.//g')
-		bestpricepence=$(echo $bestprice | sed 's/\.//g')
+		currentpricepence=$(echo $currentprice | sed -e 's/\.//g' -e 's/^0*//')
+		bestpricepence=$(echo $bestprice | sed -e 's/\.//g' -e 's/^0*//')
 		if [ $currentpricepence -lt $bestpricepence ]; then
 			printf " It's cheaper at £$currentprice!\n"
 			bestprice=$currentprice
@@ -53,8 +54,8 @@ for i in {10..99}; do
 	printf "\rTesting product code $baseproductcode$i..."
 	currentprice=$(wget -q -O - "http://cpc.farnell.com/jsp/search/productdetail.jsp?_dyncharset=UTF-8&searchTerms=$baseproductcode$i&_D%3AsearchTerms=+&%2Fpf%2Fsearch%2FTextSearchFormHandler.search=GO&_D%3A%2Fpf%2Fsearch%2FTextSearchFormHandler.search=+&s=&%2Fpf%2Fsearch%2FTextSearchFormHandler.suggestions=false&_D%3A%2Fpf%2Fsearch%2FTextSearchFormHandler.suggestions=+&%2Fpf%2Fsearch%2FTextSearchFormHandler.ref=globalsearch&_D%3A%2Fpf%2Fsearch%2FTextSearchFormHandler.ref=+&_D%3A%2Fpf%2Fsearch%2FTextSearchFormHandler.onlyInStockProducts=+&%2Fpf%2Fsearch%2FTextSearchFormHandler.onlyInStockProductsActive=true&_D%3A%2Fpf%2Fsearch%2FTextSearchFormHandler.onlyInStockProductsActive=+&_DARGS=%2Fjsp%2Fcommonfragments%2FglobalsearchE14.jsp" | grep taxedvalue -m 1 | cut -d" " -f1 | sed 's/£//g')
 	if [ "$currentprice" != "" ]; then
-		currentpricepence=$(echo $currentprice | sed 's/\.//g')
-		bestpricepence=$(echo $bestprice | sed 's/\.//g')
+		currentpricepence=$(echo $currentprice | sed -e 's/\.//g' -e 's/^0*//')
+		bestpricepence=$(echo $bestprice | sed -e 's/\.//g' -e 's/^0*//')
 		if [ $currentpricepence -lt $bestpricepence ]; then
 			printf " It's cheaper at £$currentprice!\n"
 			bestprice=$currentprice
@@ -64,4 +65,24 @@ for i in {10..99}; do
 done
 printf "\rSearch complete!                  \n\n"
 echo The cheapest product code found is $winningcode
+bestpricepence=$(echo $bestprice | sed -e 's/\.//g' -e 's/^0*//')
+savingspence=$(($originalpricepence - $bestpricepence))
+if [ "${savingspence:-0}" -gt 0 ]; then
+	savingsdigits=$(echo $savingspence | wc -c)
+	if [ "$savingsdigits" -le 3 ];
+		then
+			savingspounds=0
+		else
+			savingspounds=$(echo $savingspence | sed 's/.\{2\}$//')
+	fi		
+	savingsremainder=$(echo $savingspence | sed 's/^.*\(.\{2\}\)$/\1/')
+	if [ "$savingspounds" -eq 0 ]; then
+		if [ "$savingspence" -eq 0 ]; then
+			exit 0
+		fi
+		echo Using this code will save you $savingsremainder\p.
+		exit 0
+	fi
+	echo Using this code will save you £$savingspounds.$savingsremainder per unit.
+fi
 exit 0
