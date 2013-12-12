20ee2e05192b876aa5cf681fd34b9dc542d1d585
Gareth Halfacree <gareth@halfacree.co.uk>
2013-12-12 12:33:21 +0000

    CPC discount-finding script added.

---
+++ cpcbestprice.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Horrible nasty hacky script for finding the best price for a CPC stock item.
+# Explanation: CPC regularly sends out paper catalogues which have reduced
+# products in 'em. However, the prices on the website don't change: to get the
+# reduced pricing, you need to use a special product code only available in
+# the catalogue. These codes are the same as the full-price product codes, but
+# with a two-digit number appended.
+# This script takes a standard product code, appends a two-digit number starting
+# at 00, and searches CPC's website. If the code leads to a valid product page,
+# the price is compared to the normal price - and if it's cheaper, printed
+# to standard output. This continues until it has compared all two-digit numbers
+# through to 99 - finding you the best possible price.
+# Yes, that means 101 searches on CPC's website. Like I said, it's a horribly
+# nasty hacky script.
+
+if [ "$1" == "" ]; then
+	echo USAGE: $0 productcode
+	exit 1
+fi
+
+echo Finding best price for CPC Product Code $1...
+
+bestprice=$(wget -q -O - "http://cpc.farnell.com/jsp/search/productdetail.jsp?_dyncharset=UTF-8&searchTerms=$1&_D%3AsearchTerms=+&%2Fpf%2Fsearch%2FTextSearchFormHandler.search=GO&_D%3A%2Fpf%2Fsearch%2FTextSearchFormHandler.search=+&s=&%2Fpf%2Fsearch%2FTextSearchFormHandler.suggestions=false&_D%3A%2Fpf%2Fsearch%2FTextSearchFormHandler.suggestions=+&%2Fpf%2Fsearch%2FTextSearchFormHandler.ref=globalsearch&_D%3A%2Fpf%2Fsearch%2FTextSearchFormHandler.ref=+&_D%3A%2Fpf%2Fsearch%2FTextSearchFormHandler.onlyInStockProducts=+&%2Fpf%2Fsearch%2FTextSearchFormHandler.onlyInStockProductsActive=true&_D%3A%2Fpf%2Fsearch%2FTextSearchFormHandler.onlyInStockProductsActive=+&_DARGS=%2Fjsp%2Fcommonfragments%2FglobalsearchE14.jsp" | grep taxedvalue -m 1 | cut -d" " -f1 | cut -c 3-)
+if [ "$bestprice" == "" ]; then
+	echo Product $1 not found.
+	exit 1
+fi
+echo Base price for product $1 is £$bestprice.
+
+for i in {0..9}; do
+	searchsuffix="0$i"
+	currentprice=$(wget -q -O - "http://cpc.farnell.com/jsp/search/productdetail.jsp?_dyncharset=UTF-8&searchTerms=$1$searchsuffix&_D%3AsearchTerms=+&%2Fpf%2Fsearch%2FTextSearchFormHandler.search=GO&_D%3A%2Fpf%2Fsearch%2FTextSearchFormHandler.search=+&s=&%2Fpf%2Fsearch%2FTextSearchFormHandler.suggestions=false&_D%3A%2Fpf%2Fsearch%2FTextSearchFormHandler.suggestions=+&%2Fpf%2Fsearch%2FTextSearchFormHandler.ref=globalsearch&_D%3A%2Fpf%2Fsearch%2FTextSearchFormHandler.ref=+&_D%3A%2Fpf%2Fsearch%2FTextSearchFormHandler.onlyInStockProducts=+&%2Fpf%2Fsearch%2FTextSearchFormHandler.onlyInStockProductsActive=true&_D%3A%2Fpf%2Fsearch%2FTextSearchFormHandler.onlyInStockProductsActive=+&_DARGS=%2Fjsp%2Fcommonfragments%2FglobalsearchE14.jsp" | grep taxedvalue -m 1 | cut -d" " -f1 | cut -c 3-)
+	if [ "$currentprice" != "" ]; then
+		currentpricepence=$(echo $currentprice | sed 's/\.//g')
+		bestpricepence=$(echo $bestprice | sed 's/\.//g')
+		if [ $currentpricepence -lt $bestpricepence ]; then
+			echo Best price so far: $1\0$i at £$currentprice.
+			bestprice=$currentprice
+		fi
+	fi
+done
+
+for i in {10..99}; do
+	currentprice=$(wget -q -O - "http://cpc.farnell.com/jsp/search/productdetail.jsp?_dyncharset=UTF-8&searchTerms=$1$i&_D%3AsearchTerms=+&%2Fpf%2Fsearch%2FTextSearchFormHandler.search=GO&_D%3A%2Fpf%2Fsearch%2FTextSearchFormHandler.search=+&s=&%2Fpf%2Fsearch%2FTextSearchFormHandler.suggestions=false&_D%3A%2Fpf%2Fsearch%2FTextSearchFormHandler.suggestions=+&%2Fpf%2Fsearch%2FTextSearchFormHandler.ref=globalsearch&_D%3A%2Fpf%2Fsearch%2FTextSearchFormHandler.ref=+&_D%3A%2Fpf%2Fsearch%2FTextSearchFormHandler.onlyInStockProducts=+&%2Fpf%2Fsearch%2FTextSearchFormHandler.onlyInStockProductsActive=true&_D%3A%2Fpf%2Fsearch%2FTextSearchFormHandler.onlyInStockProductsActive=+&_DARGS=%2Fjsp%2Fcommonfragments%2FglobalsearchE14.jsp" | grep taxedvalue -m 1 | cut -d" " -f1 | cut -c 3-)
+	if [ "$currentprice" != "" ]; then
+		currentpricepence=$(echo $currentprice | sed 's/\.//g')
+		bestpricepence=$(echo $bestprice | sed 's/\.//g')
+		if [ $currentpricepence -lt $bestpricepence ]; then
+			echo Best price so far: $1$i at £$currentprice.
+			bestprice=$currentprice
+		fi
+	fi
+done
+
+echo Search complete!
