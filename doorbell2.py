df6abd1069ccfe0ff732c4f401188f421d4926bd
Gareth Halfacree <gareth@halfacree.co.uk>
2014-04-12 12:47:27 +0100

    Fixed socket descriptor error

--- doorbell2.py
+++ doorbell2.py
@@ -4,8 +4,8 @@ import RPi.GPIO as GPIO
 from twython import Twython
 import time, datetime, socket
 
-buttonPin = 23
-relayPin = 17
+buttonPin = 4 
+relayPin = 25
 screenName = 'ghalfacree'
 
 api_token = ''
@@ -14,26 +14,26 @@ access_token = ''
 access_token_secret = ''
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setwarnings(False)
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(relayPin, GPIO.OUT)
-GPIO.outout(relayPin, False)
-
-s = socket.socket()
+GPIO.output(relayPin, True)
 
 twitter = Twython(api_token, api_secret, access_token, access_token_secret)
 
 while True:
     try:
         print 'Waiting for doorbell...'
-        GPIO.wait_for_edge(buttonPin, GPIO.FALLING)
+        GPIO.wait_for_edge(buttonPin, GPIO.RISING)
         print 'There\'s somebody at the door, there\'s somebody at the door!'
         print 'Triggering office LED server...'
+        s = socket.socket()
         s.connect(('192.168.0.20', 4242))
         s.close()
         print 'LED server notified. Ringing original doorbell...'
-        GPIO.output(relayPin, True)
-        time.sleep(0.5)
         GPIO.output(relayPin, False)
+        time.sleep(0.5)
+        GPIO.output(relayPin, True)
         print 'Doorbell sounding. Sending Twitter DM...'
         twitter.send_direct_message(screen_name=screenName, text='DING-DONG at %s' %datetime.datetime.now())
         print 'Message sent. All done!'
