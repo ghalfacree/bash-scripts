76a8f3c7b617a35958f3eecba7c10b3b7be076f5
Gareth Halfacree <gareth@halfacree.co.uk>
2014-04-14 15:18:37 +0100

    Fix for phantom button pressers

--- doorbell2.py
+++ doorbell2.py
@@ -17,41 +17,47 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(relayPin, GPIO.OUT)
-GPIO.output(relayPin, True)
+GPIO.output(relayPin, False)
 
 twitter = Twython(api_token, api_secret, access_token, access_token_secret)
+
+def buttonPushed():
+    print 'GPIO activity detected, was the button pushed?'
+    time.sleep(0.01)
+    if GPIO.input(buttonPin) != GPIO.HIGH:
+        print 'False alarm.'
+        return
+    print 'There\'s somebody at the door, there\'s somebody at the door!'
+    print 'Triggering office LED server...'
+    try:
+        s = socket.socket()
+        s.settimeout(1)
+        s.connect(('192.168.0.20', 4242))
+        s.close()
+    except socket.error:
+        print 'Network error, LED server offline.'
+    print 'LED server notified. Ringing original doorbell...'
+    GPIO.output(relayPin, True)
+    time.sleep(0.5)
+    GPIO.output(relayPin, False)
+    print 'Doorbell sounding, alerting Trioptimum...'
+    try:
+        s = socket.socket()
+        s.settimeout(1)
+        s.connect(('192.168.0.50', 4242))
+        s.close()
+    except socket.error:
+        print 'Network error, Trioptimum offline.'
+    print 'Trioptimum alerted. Sending Twitter DM...'
+    #twitter.send_direct_message(screen_name=screenName, text='DING-DONG at %s' %datetime.datetime.now())
+    twitter.send_direct_message(screen_name=screenName, text='DING-DONG at %s' %datetime.datetime.today().strftime("%A %d %B %Y, %R"))
+    print 'Message sent. All done!'
 
 while True:
     try:
         print 'Waiting for doorbell...'
         GPIO.wait_for_edge(buttonPin, GPIO.RISING)
-        print 'There\'s somebody at the door, there\'s somebody at the door!'
-        print 'Triggering office LED server...'
-        try:
-            s = socket.socket()
-            s.settimeout(1)
-            s.connect(('192.168.0.20', 4242))
-            s.close()
-            print 'LED server notified.'
-        except socket.error:
-            print 'Network error, LED server offline.'
-        print 'Ringing original doorbell...'
-        GPIO.output(relayPin, False)
-        time.sleep(0.5)
-        GPIO.output(relayPin, True)
-        print 'Doorbell sounding.'
-        print 'Alerting Trioptimum...'
-        try:
-            s = socket.socket()
-            s.settimeout(1)
-            s.connect(('192.168.0.50', 4242))
-            s.close()
-            print 'Trioptimum notified.'
-        except socket.error:
-            print 'Network error, Trioptimum offline.'
-        print 'Sending Twitter DM...'
-        twitter.send_direct_message(screen_name=screenName, text='DING-DONG at %s' %datetime.datetime.now())
-        print 'Message sent. All done!'
+        buttonPushed()
         print
 
     except KeyboardInterrupt:
