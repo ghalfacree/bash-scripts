708c1a25f16a5a3b13b5bd9ddf88b4c457d09d13
Gareth Halfacree <gareth@halfacree.co.uk>
2017-10-18 15:28:12 +0100

    XPS13 palm detect hack

---
+++ xps13-palm-detect.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Script to activate palm detection on the Dell XPS 13.
+
+inputDevice = `xinput list | grep DLL075B | cut -d= -f2 | cut -f1`
+xinput set-prop $inputDevice "Synaptics Palm Dimensions" 5, 5
+xinput set-prop $inputDevice "Synaptics Palm Detection" 1
+return 0
