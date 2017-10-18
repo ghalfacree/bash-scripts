edad891f1f34cd80882ffda45fd76e56611ea463
Gareth Halfacree <gareth@halfacree.co.uk>
2017-10-18 15:29:24 +0100

    Whoops

--- xps13-palm-detect.sh
+++ xps13-palm-detect.sh
@@ -4,4 +4,4 @@
 inputDevice = `xinput list | grep DLL075B | cut -d= -f2 | cut -f1`
 xinput set-prop $inputDevice "Synaptics Palm Dimensions" 5, 5
 xinput set-prop $inputDevice "Synaptics Palm Detection" 1
-return 0
+exit 0
