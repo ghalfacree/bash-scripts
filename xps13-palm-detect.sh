d440ea2bac0fe96e3aa2c3389c3cce765f87e693
Gareth Halfacree <gareth@halfacree.co.uk>
2017-10-19 07:41:55 +0100

    Disables touchpad during typing as well as enabling palm detection

--- xps13-palm-detect.sh
+++ xps13-palm-detect.sh
@@ -3,4 +3,5 @@
 
 xinput set-prop `xinput list | grep DLL075B | cut -d= -f2 | cut -f1` "Synaptics Palm Dimensions" 5, 5
 xinput set-prop `xinput list | grep DLL075B | cut -d= -f2 | cut -f1` "Synaptics Palm Detection" 1
+syndaemon -i 0.5 -K -R -d
 exit 0
