3c0f4f427749977868b45cddf0bcf2ba43b95e14
Gareth Halfacree <gareth@halfacree.co.uk>
2018-06-20 09:48:16 +0100

    Added secondary MIME list location

--- firefoxmimefix.sh
+++ firefoxmimefix.sh
@@ -3,3 +3,4 @@
 # Fix Firefox's MIME associations.
 
 sed 's#.*octet-stream.*#application/octet-stream=gnome-open.desktop#' ~/.local/share/applications/mimeapps.list -i
+sed 's#.*octet-stream.*#application/octet-stream=gnome-open.desktop#' ~/.config/mimeapps.list -i
