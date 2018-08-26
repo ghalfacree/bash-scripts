b4a56129293b02a495d146678bcb03fee9652d45
Gareth Halfacree <gareth@halfacree.co.uk>
2018-08-26 11:21:37 +0100

    Added Google Calendar integration via GCalCLI

--- dailyschedule.sh
+++ dailyschedule.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Daily schedule printer! v0.3
+# Daily schedule printer! v0.4
 # gareth@halfacree.co.uk / https://freelance.halfacree.co.uk
-# Depends: Various defaults plus task, figlet,fortune, cowsay, and a 'net
-# connection.
+# Depends: Various defaults plus task, figlet, fortune, cowsay,
+# gcalcli with oauth configured, and a 'net connection.
 
 # This version expects a custom TaskWarrior report type: "dymo"
 # If you don't have one:
@@ -15,16 +15,17 @@
 # report.dymo.sort        project+/,entry+
 
 # Current formatting optimised for a Dymo 450 using S0929100 name cards.
-# Set CUPS to 36cpi/18lpi and a page size of 4.6x7.1cm.
+# Set CUPS to 40cpi/18lpi and a page size of 4.6x7.1cm.
 
 # Tasty variables
-LOCATION="bradford"			# Insert your location here
-SCHEDULEFILE="/tmp/dailyschedule.txt"	# Temporary file for the output
-PRINTERNAME="LabelWriter-450"		# Name of lp-compatible printer
+LOCATION="bradford"							# Insert your location here
+CALENDARS="--calendar=Home --calendar=Work --calendar=Deadlines" 	# Calendars to be used by GCalCLI
+SCHEDULEFILE="/tmp/dailyschedule.txt"					# Temporary file for the output
+PRINTERNAME="LabelWriter-450"						# Name of lp-compatible printer
 
 # The header
-date +"%A" | figlet > "$SCHEDULEFILE"
-date +"%F" | figlet >> "$SCHEDULEFILE"
+date +"%A" | figlet -f small.flf > "$SCHEDULEFILE"
+date +"%F" | figlet -f small.flf >> "$SCHEDULEFILE"
 printf "\n" >> "$SCHEDULEFILE"
 
 # Let's start with the weather...
@@ -39,13 +40,18 @@ echo -n "---------------" >> "$SCHEDULEFILE"
 task dymo >> "$SCHEDULEFILE"
 printf "\n" >> "$SCHEDULEFILE"
 
+# Calendar...
+echo "This Week's Schedule" >> "$SCHEDULEFILE"
+echo -n "--------------------" >> "$SCHEDULEFILE"
+gcalcli calw 1 $CALENDARS --nocolor --nolineart -w 9 >> "$SCHEDULEFILE"
+
 # Now a fortune cookie
 echo "What Does the Cow Say?" >> "$SCHEDULEFILE"
 echo "----------------------" >> "$SCHEDULEFILE"
 fortune -a -s | cowsay >> "$SCHEDULEFILE"
 
 # Now wrap and print...
-fold -w 65 -s "$SCHEDULEFILE" | lp -d $PRINTERNAME
+fold -w 72 -s "$SCHEDULEFILE" | lp -d $PRINTERNAME
 
 # Housekeeping
 rm "$SCHEDULEFILE"
