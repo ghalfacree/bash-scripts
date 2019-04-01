c12f47f2939d32a6cdfb6f096a07ece17efe6843
Gareth Halfacree <gareth@halfacree.co.uk>
2019-04-01 10:38:04 +0100

    Added audio support

--- hdmicapture.sh
+++ hdmicapture.sh
@@ -3,13 +3,13 @@
 
 case "$1" in
 	1080i)
-		echo "Viewing 1080i feed (Mode 12)"
+		echo "Viewing 1080i feed (Mode 13)"
 		mode=13;;
 	1080p)
-		echo "Viewing 1080p feed (Mode 15)"
+		echo "Viewing 1080p feed (Mode 10)"
 		mode=10;;
 	720p)
-		echo "Viewing 720p feed (Mode 18)"
+		echo "Viewing 720p feed (Mode 19)"
 		mode=19;;
 	*)
 	        echo "No modeline found: defaulting to 1080i (Raspberry
@@ -17,4 +17,5 @@ Pi default)"
         	mode=13;;
 esac
 
-gst-launch-1.0 -v decklinkvideosrc mode=$mode ! videoconvert ! xvimagesink
+#gst-launch-1.0 -v decklinkvideosrc mode=$mode ! videoconvert ! xvimagesink
+gst-launch-1.0 decklinkvideosrc mode=$i ! videoconvert ! xvimagesink decklinkaudiosrc typefind=true do-timestamp=true alignment-threshold=100 ! audioconvert ! autoaudiosink sync=false
