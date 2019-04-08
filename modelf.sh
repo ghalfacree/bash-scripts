6469883c1b0e54a8dcf1db71e1218ce29af67040
Gareth Halfacree <gareth@halfacree.co.uk>
2019-04-08 13:50:20 +0100

    Re-added Caps Lock toggle

--- modelf.sh
+++ modelf.sh
@@ -2,5 +2,6 @@
 
 # Key remapping for the IBM Model F PC/AT
 
-setxkbmap -option caps:super
+#setxkbmap -option caps:super
 xmodmap -e "keycode 49 = backslash bar bar bar bar"
+xmodmap -e "keycode 66 = Super_L Caps_Lock NoSymbol Caps_Lock"
