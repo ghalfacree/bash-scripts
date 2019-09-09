3efef4d2940ab66260d4b0d3f4370ed08dbc6774
Gareth Halfacree <gareth@halfacree.co.uk>
2019-09-09 22:22:08 +0100

    Truncate overly long weather status messages

--- dailyschedule.sh
+++ dailyschedule.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Daily schedule printer! v0.9
+# Daily schedule printer! v0.9.1
 # gareth@halfacree.co.uk / https://freelance.halfacree.co.uk
 # Depends: Various defaults plus task, figlet, fortune, cowsay,
 # gcalcli with oauth configured, and a 'net connection.
@@ -25,7 +25,7 @@ PRINTERNAME="LabelWriter-450"						# Name of lp-compatible printer
 date +"%a %F" | figlet -f small.flf > "$SCHEDULEFILE"
 
 # Let's start with the weather and cow...
-curl -s wttr.in/$LOCATION?0QT > "$WEATHERFILE"
+curl -s wttr.in/$LOCATION?0QT | cut -c 1-37 > "$WEATHERFILE"
 /usr/games/fortune -a -s | /usr/games/cowsay -W 30 > "$COWFILE"
 if grep --quiet "Sorry\|html" "$WEATHERFILE"; then
     printf "\nWeather service down. :(" > "$WEATHERFILE"
