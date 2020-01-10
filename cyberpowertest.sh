855f84c2205c67cf9a4c42a892ed4360856affbd
Gareth Halfacree <gareth@halfacree.co.uk>
2020-01-10 10:09:45 +0000

    Added quick UPS test tool

---
+++ cyberpowertest.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+# Script to test a CyperPower UPS via pwrstat
+# Requires pwrstat to be installed
+
+pwrstat -test &> /dev/null
+sleep 30
+
+echo UPS self-test completed with result: $(pwrstat -status | grep Result | cut -d' ' -f3-)
+
+exit 0
