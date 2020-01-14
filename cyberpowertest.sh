e3acca2f3a850d8d45184deb2d431a8bac4b0830
Gareth Halfacree <gareth@halfacree.co.uk>
2020-01-14 14:27:30 +0000

    Fixed path for cron

--- cyberpowertest.sh
+++ cyberpowertest.sh
@@ -2,7 +2,7 @@
 # Script to test a CyperPower UPS via pwrstat
 # Requires pwrstat to be installed
 
-pwrstat -test &> /dev/null
+/usr/sbin/pwrstat -test &> /dev/null
 sleep 30
 
 echo UPS self-test completed with result: $(pwrstat -status | grep Result | cut -d' ' -f3-)
