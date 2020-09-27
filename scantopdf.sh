f4604feed1d0b2c8a9ed3a9ab1408ae49dfe6382
Gareth Halfacree <gareth@halfacree.co.uk>
2020-09-27 23:23:06 +0100

    Added tool for converting PNGs to searchable text PDFs

---
+++ scantopdf.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+# Scan to PDF, a handy-dandy shell script for convering PNGs to a PDF with
+# searchable text.
+# Requires:
+#    GNU Parallel
+#    ImageMagick
+#    jpegcrush-moz (you can get this in the same place as scantopdf.sh)
+#        Everything jpgcrush-moz needs: jpegrescan, mozjpeg
+#    tesseract
+#    pdftk
+# <gareth@halfacree.co.uk>
+# https://freelance.halfacree.co.uk
+
+DPI=300
+OUTPUT="scan.pdf"
+QUALITY=85
+
+TEMPDIR=$(mktemp -d)
+
+while getopts ":r:o:q:h" FLAG; do
+    case $FLAG in
+        h )
+            echo "Script to turn PNGs into a PDF with searchable text"
+            echo "<freelance@halfacree.co.uk> / https://gareth.halfacree.co.uk"
+            echo "    Usage: scantopdf.sh -r resolution -q quality -o output"
+            echo "        -r - Scan resolution in dots per inch (DPI)"
+            echo "        -q - JPEG quality in percent"
+            echo "        -o - Filename for output PDF"
+            echo "        -h - This help"
+            exit 0
+            ;;
+        o )
+            OUTPUT="$OPTARG"
+            ;;
+        r )
+            DPI="$OPTARG"
+            case $DPI in
+                ''|*[!0-9]*)
+                    echo "ERROR: -r must be a positive integer in DPI."
+                    exit 1
+                    ;;
+            esac
+            ;;
+        q )
+            QUALITY="$OPTARG"
+            case $QUALITY in
+                ''|*[!0-9]*)
+                    echo "ERROR: -q must be a positive integer in JPEG quality."
+                    exit 1
+                    ;;
+            esac
+            if [[ $QUALITY -lt 0 || $QUALITY -gt 100 ]]; then
+                echo "ERROR: -q must be a JPEG quality percentage between 0 and 100."
+                exit 1
+            fi
+            ;;
+        \? )
+            echo "ERROR: Flag -$OPTARG not recognised."
+            echo "    Usage: scantopdf.sh -r resolution -q quality -o output"
+            echo "        -r - Scan resolution in dots per inch (DPI)"
+            echo "        -q - JPEG quality in percent"
+            echo "        -o - Filename for output PDF"
+            echo "        -h - This help"
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND -1))
+
+if [[ ! $(ls *[pPnNgG] 2>/dev/null) ]]; then
+    echo "ERROR: No PNG files found in current directory."
+    exit 1
+fi
+
+for i in parallel convert jpgcrush-moz tesseract pdftk; do
+    if [[ ! $(which $i) ]]; then
+        echo "ERROR: Dependency $i not found, please install and/or add to path."
+        exit 1
+    fi
+done
+
+echo "Found $(ls -1 *[pPnNgG] | wc -l) PNG file(s)..."
+echo "Trimming, deskewing, and converting to JPEG..."
+parallel --ungroup convert "{}" -density "$DPI"x"$DPI" -units PixelsPerInch -background black -fuzz 75% -deskew 75% -shave 25x25 -unsharp 0 -quality 85% +repage "$TEMPDIR/{.}.jpg" ::: *[pPnNgG]
+cd "$TEMPDIR"
+echo "Losslessly optimising JPEG files..."
+parallel --ungroup jpgcrush-moz "{}" &> /dev/null ::: "$TEMPDIR"/*jpg
+cd "$OLDPWD"
+echo "Performing OCR..."
+parallel -j8 --ungroup tesseract "{}" "{.}" pdf &> /dev/null ::: "$TEMPDIR"/*jpg
+echo "Creating output PDF..."
+pdftk "$TEMPDIR/"*pdf cat output "$OUTPUT"
+echo "File $OUTPUT written."
+echo "Cleaning up..."
+rm -rf "$TEMPDIR"
+echo "Finished!"
+exit 0
