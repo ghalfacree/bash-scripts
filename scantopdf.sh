36d7eb9b3f709a43608742e61762891e44d89fb7
Gareth Halfacree <gareth@halfacree.co.uk>
2020-10-01 10:04:10 +0100

    Added -t flag to pre-process to threshold black and white, -l flag to force 1-bit output

--- scantopdf.sh
+++ scantopdf.sh
@@ -19,11 +19,16 @@ BACKGROUND="white"
 DISABLEINVERSION="-c tessedit_do_invert=0"
 FORCEJPEG=0
 FORCEPNG=0
+GREYSCALE=0
+BILEVEL=0
+THRESHOLD=68
+THRESHOLDPROCESS=0
 GREYSCALECOMMAND=""
+DIRECTORYPREFIX=""
 
 TEMPDIR=$(mktemp -d)
 
-while getopts ":r:b:q:o:izjpgh" FLAG; do
+while getopts ":r:b:q:o:i:t:zjpglh" FLAG; do
     case $FLAG in
         h )
             echo "scantopdf.sh: Script to turn PNGs into a PDF with searchable text"
@@ -37,6 +42,8 @@ while getopts ":r:b:q:o:izjpgh" FLAG; do
             echo "        -j - Force JPEG imagery even if input files are grayscale"
             echo "        -p - Force PNG imagery even if input files are colour"
             echo "        -g - Force four-colour greyscale output"
+            echo "........-l - Force two-colour lineart output"
+            echo "........-t - Pre-process PNGs to two-colour"
             echo "        -z - Use Zopfli compression for PNG output (slower, smaller files)"
             echo "        -h - This help"
             exit 0
@@ -56,6 +63,26 @@ while getopts ":r:b:q:o:izjpgh" FLAG; do
         g )
             echo "FLAG -g: Forcing four-colour greyscale output."
             GREYSCALE=1
+            ;;
+        l ) 
+            echo "FLAG -l: Forcing two-colour lineart output."
+            BILEVEL=1
+            ;;
+        t ) 
+            THRESHOLD="$OPTARG"
+            THRESHOLDPROCESS=1
+            BILEVEL=1
+            case $THRESHOLD in
+                ''|*[!0-9]*)
+                    echo "ERROR: -t must be a positive integer in threshold percent."
+                    exit 1
+                    ;;
+            esac
+            if [[ $THRESHOLD -lt 0 || $THRESHOLD -gt 100 ]]; then
+                echo "ERROR: -t must be a threshold level in percent."
+                exit 1
+            fi
+            echo "FLAG -t: Pre-processing images to two-colour lineart at $THRESHOLD% threshold."
             ;;
         z )
             echo "FLAG -z: Compressing PNGs with Zopfli."
@@ -148,15 +175,28 @@ fi
 
 echo "Found $(ls -1 *[pP][nN][gG] | wc -l) PNG file(s)..."
 
+if [[ $THRESHOLDPROCESS == 1 ]]; then
+    echo "Processing PNGs into lineart at $THRESHOLD%..."
+    mkdir "$TEMPDIR/lineart"
+    parallel --ungroup convert -limit thread 1 "{}" -threshold $THRESHOLD% -depth 1 -colorspace gray "$TEMPDIR/lineart/{.}.png" ::: *[pP][nN][gG]
+    DIRECTORYPREFIX="$TEMPDIR/lineart/"
+fi
+
 if [[ ! $DPIMANUAL == 1 ]]; then
     echo "    Determining resolution from $(ls -1 *[pP][nN][gG] | head -1)..."
     DPI=$(identify -format '%x' -units PixelsPerInch "$(ls -1 *[pP][nN][gG] | head -1)")
     echo "    Using $DPI DPI. To override, cancel and run with -r resolution flag."
 fi
 
-if [[ $(identify -verbose "$(ls -1 *[pP][nN][gG] | head -1)" | grep Type | cut -d":" -f2) == " Grayscale" ]] || [[ $GREYSCALE == 1 ]]; then
+IMAGETYPE=$(identify -verbose "$(ls -1 *[pP][nN][gG] | head -1)" | grep Type | cut -d":" -f2)
+
+if [[ $IMAGETYPE == " Grayscale" || $GREYSCALE == 1 ]] && [[ ! $BILEVEL == 1 ]]; then
     echo "    Grayscale scans detected or -g flag used, setting output files to four-shade greyscale."
     GREYSCALECOMMAND="-colorspace gray -colors 4 -depth 2"
+    GREYSCALE=1
+elif [[ $IMAGETYPE == " Bilevel" ]] || [[ $BILEVEL == 1 ]]; then
+    echo "    Black and white scans detected, setting output files to 1-bit bilevel."
+    GREYSCALECOMMAND="-colorspace gray -depth 1"
     GREYSCALE=1
 fi
 
@@ -164,7 +204,7 @@ if [[ $FORCEPNG == 1 ]] || [[ $GREYSCALE == 1 && $FORCEJPEG == 0 ]]; then
     echo "    Grayscale scans detected or -p flag used; will not convert to JPEGs."
     echo "Trimming, deskewing, normalising, sharpening PNGs..."
     mkdir "$TEMPDIR"/unoptimised
-    parallel --ungroup convert -limit thread 1 "{}" -density "$DPI"x"$DPI" -units PixelsPerInch -background "$BACKGROUND" -fuzz 75% -deskew 75% -shave 25x25 -normalize -unsharp 0 $GREYSCALECOMMAND +repage "$TEMPDIR/unoptimised/{.}.png" ::: *[pP][nN][gG]
+    parallel --ungroup convert -limit thread 1 "{}" -density "$DPI"x"$DPI" -units PixelsPerInch -background "$BACKGROUND" -fuzz 75% -deskew 75% -shave 25x25 -normalize -unsharp 0 $GREYSCALECOMMAND +repage "$TEMPDIR/unoptimised/{/.}.png" ::: "$DIRECTORYPREFIX"*[pP][nN][gG]
     if [[ $ZOPFLI == 1 ]]; then
         echo "Compressing PNG files with Zopfli..."
         parallel --ungroup zopflipng "{}" "$TEMPDIR"/"{/}" &> /dev/null ::: "$TEMPDIR"/unoptimised/*png
@@ -175,7 +215,7 @@ if [[ $FORCEPNG == 1 ]] || [[ $GREYSCALE == 1 && $FORCEJPEG == 0 ]]; then
     
 else
     echo "Trimming, deskewing, normalising, sharpening, and converting to JPEG at $QUALITY% quality..."
-    parallel --ungroup convert -limit thread 1 "{}" -density "$DPI"x"$DPI" -units PixelsPerInch -background "$BACKGROUND" -fuzz 75% -deskew 75% -shave 25x25 -normalize -unsharp 0 -quality "$QUALITY"% $GREYSCALECOMMAND +repage "$TEMPDIR/{.}.jpg" ::: *[pP][nN][gG]
+    parallel --ungroup convert -limit thread 1 "{}" -density "$DPI"x"$DPI" -units PixelsPerInch -background "$BACKGROUND" -fuzz 75% -deskew 75% -shave 25x25 -normalize -unsharp 0 -quality "$QUALITY"% $GREYSCALECOMMAND +repage "$TEMPDIR/{/.}.jpg" ::: "$DIRECTORYPREFIX"*[pP][nN][gG]
     cd "$TEMPDIR"
     echo "Losslessly optimising JPEG files..."
     parallel --ungroup jpgcrush-moz "{}" &> /dev/null ::: "$TEMPDIR"/*jpg
